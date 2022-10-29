cc0c75d8630b54537f872648b27fb0f6c93a6304
Rutvik Patel <rutvik.p4t3l@gmail.com>
2022-10-29 15:54:10 -0700

    remove unnecessary import

--- src/basic.h
+++ src/basic.h
@@ -5,8 +5,6 @@
 #ifndef SEQ_BASIC_H
 #define SEQ_BASIC_H
 
-#include <string>
-
 std::tuple<int, std::string, std::string> sequence_alignment_basic(std::string seq1, std::string seq2);
 
 #endif //SEQ_BASIC_H
