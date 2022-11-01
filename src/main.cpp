5492a851feb171b2d88458604047d85fd111b53a
Rutvik Patel <rutvik.p4t3l@gmail.com>
2022-11-01 12:23:54 +0530

    add performance measure utils

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <cstring>
 #include <iostream>
 #include <tuple>
+#include <sys/time.h>
 
 #include "basic.h"
 #include "parse_file.h"
+#include "perf_measure.h"
 
 int main(int argc, char *argv[]) {
     if(argc < 2) {
@@ -17,14 +19,23 @@ int main(int argc, char *argv[]) {
     std::cout << "Base pair 1: " << base_pairs[0] << std::endl;
     std::cout << "Base pair 2: " << base_pairs[1] << std::endl;
 
-    auto result = std::tuple<int, std::string, std::string>({0, "", ""});
+    struct timeval begin, end; // store end and start time of execution
+    gettimeofday(&begin, 0); // mark start time of execution
+
+    auto result = std::tuple<int, std::string, std::string>({0, "", ""}); // store result
     if (strcmp(argv[2], "basic") == 0)
         result = sequence_alignment_basic(base_pairs[0], base_pairs[1]);
     // TODO: add mode check for sequence_alignment_efficient and get result
 
+    gettimeofday(&end, 0); // mark end time of execution
+    double total_memory = getTotalMemory(); // get memory usage
+    double total_time = getExecTime(begin, end);
+
     std::cout << std::get<0>(result) << std::endl; // Cost of a minimum cost alignment
     std::cout << std::get<1>(result) << std::endl; // Aligned sequence of string 1
     std::cout << std::get<2>(result) << std::endl; // Aligned sequence of string 2
+    std::cout << total_time << std::endl; // Total execution time
+    std::cout << total_memory << std::endl; // Total memory usage
 
     delete[](base_pairs);
     return 0;
---
+++ src/perf_measure.h
@@ -0,0 +1,33 @@
+//
+// Created by rutvik-patel on 1/11/22.
+//
+
+#ifndef SEQ_PERF_MEASURE_H
+#define SEQ_PERF_MEASURE_H
+
+#include <errno.h>
+#include <stdio.h>
+#include <sys/resource.h>
+
+extern int errno;
+
+// getrusage() is available in linux. Your code will be evaluated in Linux OS.
+long getTotalMemory() {
+    struct rusage usage;
+    int returnCode = getrusage(RUSAGE_SELF, &usage);
+    if (returnCode == 0) {
+        return usage.ru_maxrss;
+    } else {
+        // It should never occur. Check man getrusage for more info to debug.
+        // printf("error %d", errno);
+        return -1;
+    }
+}
+
+double getExecTime(struct timeval ts1, struct timeval ts2) {
+    double seconds = ts2.tv_sec - ts1.tv_sec;
+    double microseconds = ts2.tv_usec - ts1.tv_usec;
+    return seconds * 1000 + microseconds * 1e-3;
+}
+
+#endif //SEQ_PERF_MEASURE_H
