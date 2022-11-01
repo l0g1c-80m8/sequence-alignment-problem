35da6c6f19a1d4e74a75d121ed2ef6cb181abcbb
Rutvik Patel <rutvik.p4t3l@gmail.com>
2022-11-01 12:25:46 +0530

    change start time recording position

--- src/main.cpp
+++ src/main.cpp
@@ -13,14 +13,14 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    struct timeval begin, end; // store end and start time of execution
+    gettimeofday(&begin, 0); // mark start time of execution
+
     auto base_pairs = new std::string[2];
     read_parse_file(argv[1], base_pairs);
 
     std::cout << "Base pair 1: " << base_pairs[0] << std::endl;
     std::cout << "Base pair 2: " << base_pairs[1] << std::endl;
-
-    struct timeval begin, end; // store end and start time of execution
-    gettimeofday(&begin, 0); // mark start time of execution
 
     auto result = std::tuple<int, std::string, std::string>({0, "", ""}); // store result
     if (strcmp(argv[2], "basic") == 0)
